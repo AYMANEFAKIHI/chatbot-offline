b059d7bf83041849507a461c3b463242b406c77a
AYMANEFAKIHI <faymane12@gmail.com>
2025-12-06 15:53:59 +0100

    hello

--- database.js
+++ database.js
@@ -1,5 +1,5 @@
 import { SUPABASE_URL, SUPABASE_KEY } from "./config.js";
-import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
+import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
 
